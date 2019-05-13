9cdb76ceb0d4258378436ecb0d274fe7cacaf895
darkheart101 <kurokokoro@windowslive.com>
2019-05-13 07:10:31 +0300

    Forming a magic square

--- 9.FormingAMagicSquare/Solution.java
+++ 9.FormingAMagicSquare/Solution.java
@@ -10,141 +10,63 @@ public class Solution {
     
     static int formingMagicSquare(int[][] s) {
         
-        // Eight possible magic squares
-        int[][] possibleMagicSquare1 = {
-            {4,9,2},
-            {3,5,7},
-            {8,1,6}
+        int[][][] possibleMagicSquares = {
+            {
+                {4,9,2},
+                {3,5,7},
+                {8,1,6}                    
+            },
+            {
+                {2,7,6},
+                {9,5,1},
+                {4,3,8},   
+            },
+            {
+                {6,1,8},
+                {7,5,3},
+                {2,9,4},   
+            },
+            {
+                {8,3,4},
+                {1,5,9},
+                {6,7,2},   
+            },
+            {
+                {4,3,8},
+                {9,5,1},
+                {2,7,6}
+            },
+            {
+                {8,1,6},
+                {3,5,7},
+                {4,9,2},   
+            },
+            {
+                {6,7,2},
+                {1,5,9},
+                {8,3,4},   
+            },
+            {
+                {2,9,4},
+                {7,5,3},
+                {6,1,8},   
+           }            
         };
-        
-        int[][] possibleMagicSquare2 = {
-            {2,7,6},
-            {9,5,1},
-            {4,3,8},   
-       };
-        
-        int[][] possibleMagicSquare3 = {
-            {6,1,8},
-            {7,5,3},
-            {2,9,4},   
-       };        
-        
-       int[][] possibleMagicSquare4 = {
-            {8,3,4},
-            {1,5,9},
-            {6,7,2},   
-       };
-        
-        int[][] possibleMagicSquare5 = {
-            {4,3,8},
-            {9,5,1},
-            {2,7,6}
-        };
-        
-        int[][] possibleMagicSquare6 = {
-            {8,1,6},
-            {3,5,7},
-            {4,9,2},   
-       };
-        
-        int[][] possibleMagicSquare7 = {
-            {6,7,2},
-            {1,5,9},
-            {8,3,4},   
-       };        
-        
-       int[][] possibleMagicSquare8 = {
-            {2,9,4},
-            {7,5,3},
-            {6,1,8},   
-       };       
-
         
         int minCost = 0;
         int tmpCost = 50;
-        int tmp = 0;
-
-        // Test every possible magic square and find the minimum cost
+        int tmp;
         
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare1[i][j] - s[i][j]);
+        for(int possibleSquare = 0 ; possibleSquare < 8; possibleSquare++){
+            tmp = 0;
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3 ; j++){
+                    tmp += abs(possibleMagicSquares[possibleSquare][i][j] - s[i][j]);
+                }
             }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-            
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare2[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare3[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare4[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare5[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare1[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare6[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;
-
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare7[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;      
-        
-        tmp = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3 ; j++){
-                tmp += abs(possibleMagicSquare8[i][j] - s[i][j]);
-            }
-        }
-        if(tmp == 0)  tmpCost = 0;
-        if(tmp < tmpCost) tmpCost = tmp;         
+            if(tmp == 0)  tmpCost = 0;
+            if(tmp < tmpCost) tmpCost = tmp;
+        }      
         
         minCost = tmpCost;           
         
